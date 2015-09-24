b9593495eabb7e7cbbf17986ac55ad2bc6e056b6
Marble Wu <huobazi@gmail.com>
2015-09-24 20:44:26 +0800

    add change log &&  bump version

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+CHANGE LOG
+
+### v0.0.6
+
+https://github.com/huobazi/qiniu_direct_uploader/pull/2
--- lib/qiniu_direct_uploader/version.rb
+++ lib/qiniu_direct_uploader/version.rb
@@ -1,4 +1,4 @@
 # coding: utf-8
 module QiniuDirectUploader
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
